c01e6fd7fcdd9fea1a9b1fc4b60bec3f71064bf2
sathish <sathish@elumitas.com>
2025-04-11 19:12:34 +0530

    fixing bucket failure

--- server/src/helper/img_cloud_uploader.ts
+++ server/src/helper/img_cloud_uploader.ts
@@ -13,12 +13,18 @@ const BUCKET_NAME = process.env.BUCKET_NAME || ''; // replace with your bucket
 export async function uploadScreenshotToGCS(localPath: string, objectName: string): Promise<string> {
   console.log('Uploading to GCS:', localPath, objectName);
   console.log('Bucket Name:', BUCKET_NAME);
-  await storage.bucket(BUCKET_NAME).upload(localPath, {
-    destination: objectName,
-    metadata: {
-      contentType: 'image/jpeg',
-    },
-  });
+  try {
+    await storage.bucket(BUCKET_NAME).upload(localPath, {
+      destination: objectName,
+      metadata: {
+        contentType: 'image/jpeg',
+      },
+    });
+  }
+  catch (error) {
+    console.error('Error uploading to GCS:', error);
+  }
+  console.log('Upload successful:', localPath, objectName);
 
   // Return the public URL
   return `https://storage.googleapis.com/${BUCKET_NAME}/${objectName}`;
